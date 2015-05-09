8960b6d28a5f8627434c615287cc7426e4b5c8b1
Gogo <gogokaradjov@gmail.com>
2015-05-10 01:33:36 +0300

    Initial commit

---
+++ ExtendedCollections/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendedCollections
+{
+    public class Class1
+    {
+    }
+}
